d046b8a8d4cec43e35fdb7058845ecf05a29822d
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-11-24 23:07:23 -0700

    added project folder

---
+++ project/project/src/stateMachines.h
@@ -0,0 +1,12 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+void toggle_red25();
+void toggle_red75();
+void green_on_red_off();
+void dim();
+void buzzer_advance();
+void song();
+
+#endif // included

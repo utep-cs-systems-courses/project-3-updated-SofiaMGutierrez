29dd047c507dd8b1c9b712d6a13d63cfa9f9aa0b
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-12-04 09:49:51 -0700

    added song

--- wakedemo/stateMachines.h
+++ wakedemo/stateMachines.h
@@ -1,6 +1,7 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
+void xmas(int color);
 void state_advance();
 char toggle_red();
 void toggle_red25();
@@ -8,6 +9,8 @@ void toggle_red75();
 void green_on_red_off();
 void dim();
 void buzzer_advance();
+//char song(char changed);
 void song();
+void g(int s);
 
 #endif // included

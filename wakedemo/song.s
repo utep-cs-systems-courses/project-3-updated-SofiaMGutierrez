b3a7e99c6f576eb6db5d87638f2042e0ac881431
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-12-04 09:37:00 -0700

    changed method

--- wakedemo/song.s
+++ wakedemo/song.s
@@ -1,39 +1,51 @@
+
 	.arch msp430g2553
 	.p2align 1,0
-state:
-	.word 1			;
-note:
-	.byte 0			;
-jt:
-	.word case0		;
-	.word case1		;
-	.word case2		;
-	.word case3		;
-	.word default		;
 
+	.data
+s:
+	.word 1			; static int s = 1
+
+	.data 
+n:
+	.word 0			; static int n = 0
+
+	.text
+jt:
+	.word option1		; jt[0]
+	.word option2		; jt[1]
+	.word option3		; jt[2]
+
+	.text
 	.global song
 song:
-	mov state, r12		;
-	cmp #3, r12		;state-3
-	jnc default
-	add r12, r12		;
-	mov jt(r12), r0		;
+	;; range check on selector (s)
+	cmp #4, &s		; s-4 doesn't borrow if s>3
+	jhs end			; jmp if s>3
 
-case1:
-	mov #1000, &note	;
-	add #1, &state		;
-	jmp end			;
-case2:
-	mov #2000, &note	;
-	add #1, &state		;
-	jmp end			;
-case3:
-	move #3000, &note	;
-	add #1, &state		;
-	jmp end			;
-default:
-	add #0, &note		;
-	jmp end			;
+	;; index into jt
+	mov &s, r12
+	add r12, r12		; r12 = 2 bytes * s
+	mov jt(r12), r0		; jmp jt[s]
+
+	;; switch table options
+	;; same order as in source code
+option1:
+	mov #1000, &n
+	add #1, &s
+	jmp g
+	
+option2:
+	mov #2000, &n
+	add #1, &s
+	jmp g
+option3:
+	mov #3000, &n
+	mov #1, &s
+	jmp g
+g:
+	mov &n, r12		; g(n)
+	call #g
+	jmp end
 end:
-	call #buzzer_set_period	;
-	pop r0			;
+	pop r0			; return to RA

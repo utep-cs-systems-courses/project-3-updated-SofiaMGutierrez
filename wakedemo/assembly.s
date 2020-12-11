630a52431b97b01a7791dcfa694fc3ec45562eb1
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-12-11 03:40:03 -0700

    final push

--- wakedemo/assembly.s
+++ wakedemo/assembly.s
@@ -3,7 +3,7 @@
 	.p2align 1, 0
 	.text
 
-	.global buzz_advance_assembly
+	.global buzz_advance_assembly ; makes it visible to other object files that are visible to it
 
 buzz_advance_assembly:
 	;; note is stored in r12 since its sent by parameter
@@ -11,14 +11,15 @@ buzz_advance_assembly:
 	sub #2, R1		; allocate memory for short
 	mov #0, 0(R1)		; cmp_var = 0
 
-	cmp r12, 0(R1)		; if(note < cmp_var), (note != cmp_var)
-	jl fi			; jump to fi if note is not less than cmp_var
-	add #100, r12		; if note < cmp_var then add 100 to note
+	cmp r12, 0(R1)		; if(note < cmp_var)
+	jl fi			; N != V
+	add #1, r12		; if note < cmp_var then add 1 to note
 	jmp end
 fi:
-	add #200, r12		; else add 200 to note
+	add #0, r12		; else add 0 to note
 	jmp end
 
 end:
 	add #2, R1		; deallocate memory for short
-	pop r0			; return value of r12
+	;; note will be returned
+	pop r0			; return the value of R12
--- wakedemo/song.s
+++ wakedemo/song.s
@@ -2,50 +2,49 @@
 	.arch msp430g2553
 	.p2align 1,0
 
-	.data
+	.data			; s is a static variable
 s:
-	.word 1			; static int s = 1
+	.word 1			; s = 1
 
-	.data 
-n:
-	.word 0			; static int n = 0
-
+	.data
+note1:
+	.word 0			; note = 0;
 	.text
 jt:
 	.word option1		; jt[0]
 	.word option2		; jt[1]
 	.word option3		; jt[2]
-
+	
 	.text
 	.global song
 song:
 	;; range check on selector (s)
-	cmp #4, &s		; s-4 doesn't borrow if s>3
-	jhs end			; jmp if s>3
+	cmp #3, &s		; s-3 doesn't borrow if s > 2
+	jhs end			; jmp if s > 2
 
 	;; index into jt
-	mov &s, r12
-	add r12, r12		; r12 = 2 bytes * s
-	mov jt(r12), r0		; jmp jt[s]
+	mov &s, R12
+	add R12, R12		; r12 = 2 bytes * s
+	mov jt(R12), R0		; jmp jt[s]
 
 	;; switch table options
 	;; same order as in source code
 option1:
-	mov #1000, &n
+	mov #1000, &note1
 	add #1, &s
-	jmp g
+	jmp buzzer
 	
 option2:
-	mov #2000, &n
+	mov #2000, &note1
 	add #1, &s
-	jmp g
+	jmp buzzer
 option3:
-	mov #3000, &n
+	mov #3000, &note1
 	mov #1, &s
-	jmp g
-g:
-	mov &n, r12		; g(n)
-	call #g
+	jmp buzzer
+buzzer:
+	mov &note1, R12		; play_buzzer(note1)
+	call #play_buzzer
 	jmp end
 end:
 	pop r0			; return to RA
